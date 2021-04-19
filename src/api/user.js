8395f0c451af4389317e8c6024d7e70b85f95a08
Youwei_star <1176071733@qq.com>
2021-04-19 15:04:53 +0800

    封装了登录请求模块 by Youwei

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,15 @@
-// import request from "@/utils/request"
+import request from '@/utils/request'
 
-export function login(data) {}
+/**
+ * 封装登录请求模块
+ */
+export function login(data) {
+  return request({
+    url: '/sys/login',
+    method: 'post',
+    data
+  })
+}
 
 export function getInfo(token) {}
 
