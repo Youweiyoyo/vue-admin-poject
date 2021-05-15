edd1cb59be7d94df1fccae52604890b582372a6c
yangbo <yoyoyo9527@163.com>
2021-05-16 00:16:24 +0800

    组织架构页面init by Youwei

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <el-card>
+        <el-row type="flex" justify="space-between">
+          <el-col>
+            <span>JavaScript高级程序设计</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row type="flex" justify="end">
+              <el-col>负责人</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span>
+                    下拉菜单
+                    <i class="el-icon-bottom" />
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>黄金糕</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
