7d2f0b91c6d5b05a5281f55066e027d192c443d9
yangbo <yoyoyo9527@163.com>
2021-05-16 14:01:43 +0800

    绘制组值架构树形结构 by Youwei

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <el-card>
-        <el-row type="flex" justify="space-between">
+      <el-card class="tree-card">
+        <el-row
+          type="flex"
+          justify="space-between"
+          align="middle"
+          style="height: 40px"
+        >
           <el-col>
             <span>JavaScript高级程序设计</span>
           </el-col>
@@ -16,20 +21,72 @@
                     <i class="el-icon-bottom" />
                   </span>
                   <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>黄金糕</el-dropdown-item>
+                    <el-dropdown-item>添加子部门</el-dropdown-item>
+                    <el-dropdown-item>编辑部门</el-dropdown-item>
+                    <el-dropdown-item>删除部门</el-dropdown-item>
                   </el-dropdown-menu>
                 </el-dropdown>
               </el-col>
             </el-row>
           </el-col>
         </el-row>
+        <el-tree :data="departs" :props="defaultProps">
+          <el-row
+            slot-scope="{ data }"
+            type="flex"
+            justify="space-between"
+            style="width:100% ;height:50px"
+            align="middle"
+          >
+            <el-col>
+              <span>{{ data.name }}</span>
+            </el-col>
+            <el-col :span="4">
+              <el-row type="flex" justify="end">
+                <el-col>负责人</el-col>
+                <el-col>
+                  <el-dropdown>
+                    <span>
+                      下拉菜单
+                      <i class="el-icon-bottom" />
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item>编辑部门</el-dropdown-item>
+                      <el-dropdown-item>删除部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.tree-card {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
