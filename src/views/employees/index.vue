b8babb9562cf7afdedd0e95516e878ab2ec44245
yangbo <yoyoyo9527@163.com>
2021-05-23 16:16:38 +0800

    员工管理init by Youwei

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <page-tools :show-before="false">
+        <template #after>
+          <el-button type="primary">导入</el-button>
+          <el-button type="primary" icon="el-icon-plus">新增</el-button>
+        </template>
+      </page-tools>
     </div>
   </div>
 </template>
