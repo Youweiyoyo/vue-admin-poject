b237918444269b12751092144a06bba1e1581c75
yangbo <yoyoyo9527@163.com>
2021-06-06 13:57:33 +0800

    路由上传组件封装完毕 by Youwei

---
+++ src/views/import/index.vue
@@ -0,0 +1,23 @@
+<!-- 公共路由组件--导入文件组件 -->
+<template>
+  <div>
+    <upload-excel />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss"></style>
