2d3a461c86a96aadd4012dd060130ada7f2110f9
yangbo <yoyoyo9527@163.com>
2021-06-26 11:55:46 +0800

    给用户分配角色功能完成 by Youwei

--- src/views/employees/components/assign-role.vue
+++ src/views/employees/components/assign-role.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="assign-role">
-    <el-dialog :visible="showRoleDialog" title="分配角色" width="30%">
+    <el-dialog :visible="showRoleDialog" title="分配角色" width="30%" @close="cancel">
       <el-checkbox-group v-model="checkList">
         <el-checkbox v-for="item in list" :key="item.id" :label="item.id">{{
           item.name
@@ -9,8 +9,12 @@
       <template #footer>
         <el-row type="flex" justify="center">
           <el-col :span="6">
-            <el-button size="small" type="default">取消</el-button>
-            <el-button size="small" type="primary">确定</el-button>
+            <el-button size="small" type="default" @click="cancel">取消</el-button>
+            <el-button
+              size="small"
+              type="primary"
+              @click="saveRole"
+            >确定</el-button>
           </el-col>
         </el-row>
       </template>
@@ -19,6 +23,7 @@
 </template>
 
 <script>
+import { assignRoles } from '@/api/employees'
 import { getRoleList } from '@/api/setting'
 import { getUserDetailById } from '@/api/user'
 export default {
@@ -57,6 +62,17 @@ export default {
     async getUserDetailById(id) {
       const { roleIds } = await getUserDetailById(id)
       this.checkList = roleIds
+    },
+    // 确定
+    async saveRole() {
+      await assignRoles({ id: this.userId, roleIds: this.checkList })
+      this.$message.success('保存用户权限成功')
+      this.$emit('update:showRoleDialog', false)
+    },
+    // 取消
+    cancel() {
+      this.checkList = []
+      this.$emit('update:showRoleDialog', false)
     }
   }
 }
