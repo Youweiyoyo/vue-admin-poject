e305548b9fb0d2efaec0b855fa65645e58d54d15
yangbo <yoyoyo9527@163.com>
2021-06-26 12:22:54 +0800

    权限接口封装完成 by Youwei

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限管理
-      </h2>
+      <el-card>
+        <el-row type="flex" justify="flex-end">
+          <el-button type="primary">添加权限</el-button>
+        </el-row>
+        <vxe-table border>
+          <vxe-table-column title="菜单名称" />
+          <vxe-table-column title="权限标识" />
+          <vxe-table-column title="描述" />
+          <vxe-table-column title="操作">
+            <template v-slot="{ row }">
+              <el-button type="text">添加权限点</el-button>
+              <el-button type="text">查看APi权限</el-button>
+              <el-button type="text">查看</el-button>
+              <el-button type="text">删除</el-button>
+            </template>
+          </vxe-table-column>
+        </vxe-table>
+      </el-card>
     </div>
   </div>
 </template>
