cdabe33847d77c4f1c95d7481d8704786a6321fb
yangbo <yoyoyo9527@163.com>
2021-07-11 22:48:20 +0800

    按钮级别受控权限功能完成 by Youwei

--- src/mixins/checkPermission.js
+++ src/mixins/checkPermission.js
@@ -1,7 +1,9 @@
 import store from '@/store'
 export default {
   methods: {
+    // 检查权限 key 为需要检查的权限
     checkPermission(key) {
+      // 去用户的信息里面找,如果 points 中如果有 key 就证明用户有该按钮的权限
       const { userInfo } = store.state.user
       if (userInfo.roles && userInfo.roles.points) {
         return userInfo.roles.points.some(item => item === key)
