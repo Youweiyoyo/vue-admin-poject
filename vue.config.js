8e7fcbd72adcfe51a088c6863da5d81e33380f13
yangbo <yoyoyo9527@163.com>
2021-07-24 17:13:53 +0800

    使用CDN资源 by Youwei

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,31 @@ const name = defaultSettings.title || 'vue Admin Template' // page title
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+let CDN = { css: [], js: [] }
+let externals = {}
+const isProd = process.env.NODE_ENV === 'production' // 判断是否是生产环境
+if (isProd) {
+  // 只有生产环境,才有必要去做排除和 CDN 注入
+  externals = {
+    'element-ui': 'ELEMENT',
+    xlsx: 'XLSX',
+    vue: 'Vue'
+  }
+  CDN = {
+    css: [
+      // element-ui css
+      'https://unpkg.com/element-ui/lib/theme-chalk/index.css' // 样式表
+    ],
+    js: [
+      // vue must at first!
+      'https://unpkg.com/vue/dist/vue.js', // vuejs
+      // element-ui js
+      'https://unpkg.com/element-ui/lib/index.js', // elementUI
+      'https://cdn.jsdelivr.net/npm/xlsx@0.16.6/dist/jszip.min.js',
+      'https://cdn.jsdelivr.net/npm/xlsx@0.16.6/dist/xlsx.full.min.js'
+    ]
+  }
+}
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
@@ -54,7 +79,12 @@ module.exports = {
       alias: {
         '@': resolve('src')
       }
-    }
+    },
+    // 要排除的包名
+    // 'key':'value    key是要排除的包名 value是引入引入的包的全局变量
+    // 因为要排除 elementUI 后面使用 CDN 资源 CDN 资源中存在包的全局变量 ELEMENT名
+    // externals会首先排除掉定义的包名，空出来的位置使用全局变量来替换
+    externals: externals
   },
   chainWebpack(config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
@@ -67,7 +97,13 @@ module.exports = {
         include: 'initial'
       }
     ])
-
+    // 注入 CDN 变量
+    // 在执行打包的时候将 cdn 变量注入到 html 模板中
+    config.plugin('html').tag(args => {
+      // args 是 html 模板的一个变量
+      args[0].cdn = CDN // 后面 cdn 就是定义的变量
+      return args // 返回这个参数
+    })
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
 
